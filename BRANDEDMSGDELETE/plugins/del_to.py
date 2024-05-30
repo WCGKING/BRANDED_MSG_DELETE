05e3776145a2ab4844f3a165b91affaccaa844f4
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-30 16:02:15 +0530

    Create del_to.py

---
+++ BRANDEDMSGDELETE/plugins/del_to.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) WCGKING
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.errors import (
+    ChatAdminRequired
+)
+from bot import (
+    AKTIFPERINTAH,
+    DEL_TO_COMMAND
+)
+from BRANDEDMSGDELETE.BRANDEDMSGDELETE import BRANDEDMSGDELETE
+from BRANDEDMSGDELETE.helpers.custom_filter import allowed_chat_filter
+
+
+@BRANDEDMSGDELETE.on_message(
+    filters.command(DEL_TO_COMMAND) &
+    filters.reply &
+    allowed_chat_filter
+)
+async def del_to_command_fn(client: Bot, message: Message):
+    try:
+        status_message = await message.reply_text(
+            text="trying to save ending message_id"
+        )
+    except ChatAdminRequired:
+        status_message = None
+    if message.chat.id not in AKTIFPERINTAH:
+        AKTIFPERINTAH[message.chat.id] = {}
+    AKTIFPERINTAH[
+        message.chat.id
+    ][
+        DEL_TO_COMMAND
+    ] = message.reply_to_message.id
+    if status_message:
+        await status_message.edit_text(
+            text=(
+                "saved ending message_id. "
+                "https://github.com/WCGKING/BRANDED_MSG_DELETE"
+            )
+        )
+        await status_message.delete()
+    await message.delete()
