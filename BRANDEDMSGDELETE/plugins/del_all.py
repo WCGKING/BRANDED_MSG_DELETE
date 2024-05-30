0123239238429bcb0bfaf8fee2ccf3ceaa0727f7
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-30 15:58:18 +0530

    Create del_all.py

---
+++ BRANDEDMSGDELETE/plugins/del_all.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) WCGKING
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from pyrogram import filters
+from pyrogram.errors import (
+    ChatAdminRequired
+)
+from pyrogram.raw.functions.channels import DeleteHistory
+from pyrogram.types import (
+    Message,
+    LinkPreviewOptions
+)
+from BRANDEDMSGDELETE import (
+    BEGINNING_DEL_ALL_MESSAGE,
+    DEL_ALL_COMMAND,
+    IN_CORRECT_PERMISSIONS_MESSAGE,
+    SHTL_BOT_HCAT_QO,
+    SHTL_USR_HCAT_QO,
+    THANK_YOU_MESSAGE
+)
+from BRANDEDMSGDELETE.BRANDEDMSGDELETE import BRANDEDMSGDELETE
+from BRANDEDMSGDELETE.helpers.custom_filter import allowed_chat_filter
+from BRANDEDMSGDELETE.helpers.get_messages import get_messages
+from BRANDEDMSGDELETE.helpers.make_user_join_chat import make_chat_user_join
+
+
+@BRANDEDMSGDELETE.on_message(
+    filters.command(DEL_ALL_COMMAND) &
+    allowed_chat_filter
+)
+async def del_all_command_fn(client: Bot, message: Message):
+    try:
+        status_message = await message.reply_text(
+            text=BEGINNING_DEL_ALL_MESSAGE
+        )
+    except ChatAdminRequired:
+        status_message = None
+
+    s__, nop = await make_chat_user_join(
+        client.USER,
+        client.USER_ID,
+        message
+    )
+    if not s__:
+        if status_message:
+            await status_message.edit_text(
+                text=IN_CORRECT_PERMISSIONS_MESSAGE.format(
+                    nop
+                ),
+                link_preview_options=LinkPreviewOptions(
+                    is_disabled=True
+                ),
+            )
+        else:
+            await message.delete()
+        return
+
+    if (
+        # don't know a better way :\
+        str(message.chat.id).startswith("-100") and
+        # only creator of group can do this
+        s__ and
+        nop == 140
+    ):
+        await client.USER.send(
+            DeleteHistory(
+                for_everyone=True,
+                channel=(
+                    await client.USER.resolve_peer(
+                        message.chat.id
+                    )
+                ),
+                max_id=0
+            )
+        )
+
+    else:
+        await get_messages(
+            client.USER,
+            message.chat.id,
+            0,
+            status_message.id if status_message else message.id,
+            []
+        )
+
+        # leave the chat, after task is done
+        if SHTL_USR_HCAT_QO:
+            await client.USER.leave_chat(message.chat.id)
+        if SHTL_BOT_HCAT_QO:
+            await client.leave_chat(message.chat.id)
+
+    # edit with channel message ads,
+    # after process is completed
+    if status_message:
+        await status_message.edit_text(
+            text=THANK_YOU_MESSAGE
+        )
